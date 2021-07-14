da381848e8ecb953c9d34a760c0403ea7a811ec6
ShjnNg <shjnk7651@gmail.com>
2021-07-14 11:34:41 +0700

    change vendor

--- src/components/Vendor/index.js
+++ src/components/Vendor/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import "./index.css";
 // import Slideshow from '../SlideShow/SlideShow';
 // import OwlCarousel from 'react-owl-carousel';
 // import 'owl.carousel/dist/assets/owl.carousel.css';
